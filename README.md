23b21910fd124eecce126a4e81e8a12f501d1ed2
adiferrer <adiferrer@gmail.com>
2022-08-22 17:43:23 -0600

    Add changes

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - Got it working for single operator expressions
 - Clear Button is Working
+
+TODO:
+- get backspace button to work
+- add keyboard assist
+- 
--- js-file.js
+++ js-file.js
@@ -1,3 +1,17 @@
+const display = document.getElementById('input-box');
+const digitButtons = Array.from(document.querySelectorAll('.digits'))
+                          .sort((a, b) => Number(a.textContent) > Number(b.textContent) ? 1 : -1);
+const operatorButtons = Array.from(document.querySelectorAll('.operators'));
+const equalButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
+const backspaceButton = document.getElementById('delete');
+const decimalButton = document.getElementById('decimal-point');
+let previousValue = '';
+let currentValue = '';
+let currentOperator = '';
+let fullExpression = [];
+let total = '';
+
 // Below are the basic arithmetic functions
 function add(a, b) {
     return a + b;
@@ -24,106 +38,101 @@ function operate(a, b, operator) {
     else if (operator === "÷") return divide(a, b);
 }
 
-const display = document.getElementById('input-box');
-const digitButtons = Array.from(document.querySelectorAll('.digits'))
-                          .sort((a, b) => Number(a.textContent) > Number(b.textContent) ? 1 : -1);
-const operatorButtons = Array.from(document.querySelectorAll('.operators'));
-const equalButton = document.getElementById('equals');
-const clearButton = document.getElementById('clear');
-const decimalButton = document.getElementById('decimal-point');
-let firstValue = '';
-let secondValue = '';
-let currentOperator = '';
-let fullExpression = [];
-let total = '';
-
-for (let d = 0; d < digitButtons.length; d++) {
-    digitButtons[d].addEventListener('click', () => {
-        if (currentOperator == '') firstValue += d;
-        else if (firstValue != '' && currentOperator != '' && fullExpression.indexOf('=') < 0) {
-            if (secondValue == '') display.textContent = '';
-            for (let c = 0; c < operatorButtons.length; c++) {
-                if (operatorButtons[c].classList.contains('operators-clicked')) {
-                    operatorButtons[c].classList.remove('operators-clicked');
-                    break;
-                }
-            }
-            secondValue += d;
-            if (fullExpression.length % 2 != 0) fullExpression.push(currentOperator);
-        } else if (fullExpression.indexOf('=') >= 0) {
-            display.textContent = '';
-            if (secondValue != '') {
-                firstValue = '' + d;
-                currentOperator = '';
-                secondValue = '';
-                fullExpression = [];
-            } else {
-                secondValue += d;
-            }
+function removeTransition() {
+    for (op of operatorButtons) {
+        if (op.classList.contains('operators-clicked')) {
+            op.classList.remove('operators-clicked');
+            break;
         }
+    }
+}
 
-        // add code to make the 0 button work accordingly
+digitButtons.forEach(digit => digit.addEventListener('click', () => {
+    const d = digit.textContent;
 
+    if (currentOperator == '') previousValue += d;
+    else {
+        if (previousValue != '' && fullExpression.indexOf('=') < 0) {
+            if (currentValue == '') display.textContent = '';
+       
+            currentValue += d;
+            if (fullExpression.length % 2 != 0) fullExpression.push(currentOperator);
+        }
+    }
+    
+    if (fullExpression.indexOf('=') >= 0) {
+        display.textContent = '';
+        if (currentValue != '') {
+            previousValue = '' + d;
+            currentOperator = '';
+            currentValue = '';
+            fullExpression = [];
+        } else {
+            currentValue += d;
+        }
+    }
+
+    if (display.textContent == '0') {
+        if (d == 0) document.getElementById("zero").disabled = true;
+        else {
+            document.getElementById("zero").disabled = false;
+            display.textContent = '' + d;
+        }
+    } else {
         if (display.textContent.length == 13) {
             display.textContent = display.textContent.substr(0, 13);  
         } else {
             display.textContent += d;
         }
-    });
-}
+    }
+}))
 
-// add code for the decimal point event listener
 decimalButton.addEventListener('click', () => {
     if (display.textContent.indexOf('.') < 0) {
-        if (currentOperator == '' && firstValue.indexOf('.') < 0) {
-            if (firstValue == '') {
-                firstValue += '0';
+        if (currentOperator == '' && previousValue.indexOf('.') < 0) {
+            if (previousValue == '') {
+                previousValue += '0';
             } 
-            firstValue += '.'; 
-        } else if (currentOperator != '' && secondValue.indexOf('.') < 0) {
-            if (secondValue == '') {
-                secondValue += '0';
+            previousValue += '.'; 
+        } else if (currentOperator != '' && currentValue.indexOf('.') < 0) {
+            if (currentValue == '') {
+                currentValue += '0';
             }
-            secondValue += '.'; 
+            currentValue += '.'; 
         }
         display.textContent += '.';
     }
 });
 
-for (let o = 0; o < operatorButtons.length; o++) {
-    operatorButtons[o].addEventListener('click', () => {
-        for (let c = 0; c < operatorButtons.length; c++) {
-            if (operatorButtons[c].classList.contains('operators-clicked')) {
-                operatorButtons[c].classList.remove('operators-clicked');
-                break;
-            }
-        }
+operatorButtons.forEach(operator => operator.addEventListener('click', () => {
+    removeTransition();
 
-        if (fullExpression.length == 0 && firstValue != '') fullExpression.push(Number(firstValue));
-        else if (fullExpression.indexOf('=') < 0) {
-            fullExpression.push(Number(secondValue));
-            total = operate(Number(firstValue), Number(secondValue), currentOperator);
-            firstValue = total;
-            secondValue = '';
-            display.textContent = String(total).substr(0, 13);
-        } else if (fullExpression.lastIndexOf('=') == fullExpression.length - 2) secondValue = '';
-        operatorButtons[o].classList.add('operators-clicked');
-        if (firstValue != '') currentOperator = operatorButtons[o].textContent;
-    });
-}
+    if (fullExpression.length == 0 && previousValue != '') fullExpression.push(Number(previousValue));
+    else if (fullExpression.lastIndexOf('=') < 0) {
+        fullExpression.push(Number(currentValue));
+        total = operate(Number(previousValue), Number(currentValue), currentOperator);
+        previousValue = total;
+        currentValue = '';
+        display.textContent = String(total).substr(0, 13);
+    } else if (fullExpression.lastIndexOf('=') == fullExpression.length - 2) currentValue = '';
+    operator.classList.add('operators-clicked');
+    if (previousValue != '') currentOperator = operator.textContent;
+}))
 
 equalButton.addEventListener('click', () => {
-    if (secondValue != '') {
+    removeTransition();
+
+    if (currentValue != '') {
         if (fullExpression.indexOf('=') >= 0) {
-            fullExpression.push(Number(firstValue));
+            fullExpression.push(Number(previousValue));
             fullExpression.push(currentOperator);
         }
-        total = operate(Number(firstValue), Number(secondValue), currentOperator);
-        fullExpression.push(Number(secondValue));
+        total = operate(Number(previousValue), Number(currentValue), currentOperator);
+        fullExpression.push(Number(currentValue));
         fullExpression.push('=');
         fullExpression.push(total);
         display.textContent = String(total).substr(0, 13);
-        firstValue = total;
+        previousValue = total;
     }
 });
 
@@ -131,9 +140,14 @@ clearButton.addEventListener('click', () => {
     const confirm = prompt('Are you sure? Y or N').toLowerCase(); 
     if (confirm == "yes" || confirm == 'y') {
         display.textContent = '';
-        firstValue = '';
+        previousValue = '';
         currentOperator = '';
-        secondValue = '';
+        currentValue = '';
         fullExpression = [];
     }
 });
+
+// backspaceButton.addEventListener('click', () => {
+//     const currentContent = display.textContent;
+//     display.textContent = currentContent.slice(0, currentContent.length-2);
+// })
